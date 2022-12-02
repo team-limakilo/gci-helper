f9e4e020832045958b0b8905acd5ec58de1ec859
Ricardo Maes <ricmzn@gmail.com>
2022-12-02 20:19:12 -0300

    Avoid accidental toast on scroll

--- src/routes/components/Hint.svelte
+++ src/routes/components/Hint.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
     export let title: string;
 
-    let touchId = 0;
+    let touchId = null;
+    let touchX = 0;
+    let touchY = 0;
+
+    function distance(x1: number, y1: number, x2: number, y2: number) {
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 
     function onTouchStart(event: TouchEvent) {
-        touchId = event.targetTouches[0].identifier;
+        const touch = event.targetTouches[0];
+        touchId = touch?.identifier;
+        touchX = touch?.clientX;
+        touchY = touch?.clientY;
     }
 
     function onTouchEnd(event: TouchEvent) {
-        if (event.changedTouches[0].identifier == touchId) {
-            window.toasts.set([{ text: title, time: event.timeStamp }]);
+        const touch = event.changedTouches[0];
+        if (
+            touch?.identifier === touchId &&
+            distance(touchX, touchY, touch?.clientX, touch?.clientY) < 10
+        ) {
+            const target = document.elementFromPoint(
+                touch.clientX,
+                touch.clientY
+            );
+            if (target == event.target) {
+                window.toasts.set([{ text: title, time: event.timeStamp }]);
+            }
         }
     }
 </script>
