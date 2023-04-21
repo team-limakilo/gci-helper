1c15874bdf250f9481a9a76323fbfe297caf2c2f
Dmytro Smirnov <dmytro.s@finixio.com>
2023-04-21 04:45:04 +0300

    Add statistics page

---
+++ src/routes/statistics/+page.svelte
@@ -0,0 +1,549 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+    import dayjs from "dayjs";
+    import relativeTime from "dayjs/plugin/relativeTime.js";
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import Toast from "../components/Toast.svelte";
+    import type { ClientData } from "../data/types";
+    import Select from 'svelte-select';
+
+    dayjs.extend(relativeTime);
+
+    let interval: NodeJS.Timer;
+    let data: any;
+    let toasts: ToastData[] = [];
+    let error = "";
+    let pilots = [];
+    let airframeTimes = {}
+    const general = {
+        topAa: {
+            name: '',
+            kills: ''
+        },
+        topAg: {
+            name: '',
+            kills: ''
+        },
+        topAirframe: {
+            name: '',
+            time: ''
+        },
+        topSorties: {
+            name: '',
+            amount: ''
+        },
+    }
+
+    async function updateData() {
+        async function fetchData(): Promise<ClientData | Error> {
+            const response = await fetch(`https://kestrelsnest.co.ua/stats`).catch((e) => {
+                console.log(e)
+                return new Error("Could not connect to server");
+            });
+            if (response instanceof Error) {
+                return response;
+            }
+            if (response.status >= 200 && response.status < 400) {
+                return response.json().catch((err) => {
+                    console.error(err);
+                    return new Error("Could not understand data from server");
+                });
+            } else {
+                return new Error("The server returned an error");
+            }
+        }
+
+        const result = await fetchData();
+        if (result instanceof Error) {
+            error = `Error: ${result.message}`;
+        } else {
+            error = "";
+            data = result;
+            pilots = [];
+
+            Object.keys(data.stats).forEach(p => {
+                if (!data.stats[p].times) return;
+
+                const times = data.stats[p].times;
+                const name = data.stats[p].names['1'];
+                const airframe = Object.keys(times).sort((a, b) => {
+                    return times[b].inAir - times[a].inAir;
+                })
+                const totalTimeSeconds = Object.keys(times).reduce((a, b) => {
+                    return a + times[b].inAir;
+                }, 0);
+                const totalAa = Object.keys(times).reduce((a, b) => {
+                    if(times[b].kills?.Planes?.total) {
+                        return a + times[b].kills.Planes.total;
+                    }
+                    return a;
+                }, 0);
+                const totalAg = Object.keys(times).reduce((a, b) => {
+                    if(times[b].kills && times[b].kills['Ground Units']?.total) {
+                        return a + times[b].kills['Ground Units'].total;
+                    }
+                    return a;
+                }, 0);
+                const totalLoses = Object.keys(times).reduce((a, b) => {
+                    if(times[b].actions?.losses?.crash) {
+                        return a + times[b].actions.losses.crash;
+                    }
+                    return a;
+                }, 0);
+
+                const totalTime = new Date(totalTimeSeconds*1000).toISOString().slice(11, 19);
+
+                pilots.push({
+                    name,
+                    airframe: airframe[0],
+                    airframeSeconds: times[airframe[0]].inAir,
+                    totalTime,
+                    totalAa,
+                    totalAg,
+                    totalLoses
+                })
+            });
+            
+            if (pilots.length) {
+
+                const generalAa = pilots.sort((a, b) => {
+                    return b.totalAa - a.totalAa;
+                });
+                general.topAa.name = generalAa[0].name;
+                general.topAa.kills = generalAa[0].totalAa;
+
+                const generalAg = pilots.sort((a, b) => {
+                    return b.totalAg - a.totalAg;
+                });
+                general.topAg.name = generalAg[0].name;
+                general.topAg.kills = generalAg[0].totalAg;
+
+                airframeTimes = {}
+                pilots.forEach(p => {
+                    if (airframeTimes[p.airframe]) {
+                        airframeTimes[p.airframe].time += p.airframeSeconds;
+                    } else {
+                        airframeTimes[p.airframe] = {
+                            name: p.airframe,
+                            time: p.airframeSeconds,
+                        }
+                    }
+                });
+                const generalAirframe:any = Object.values(airframeTimes).sort((a:any, b:any) => {
+                    return b.time - a.time;
+                })
+
+                general.topAirframe.name = generalAirframe[0].name;
+                general.topAirframe.time = new Date(generalAirframe[0].time*1000).toISOString().slice(11, 19);
+            }
+        }
+    }
+
+    onMount(() => {
+        updateData();
+        window.toasts = writable([]);
+        window.toasts.subscribe((value: ToastData[]) => (toasts = value));
+        interval = setInterval(updateData, 10 * 1000);
+        return () => clearInterval(interval);
+    });
+</script>
+
+<svelte:head>
+    {#if data != null}
+        <title>Campaign Statistics</title>
+    {/if}
+</svelte:head>
+
+<div class="warning {error.length > 0 ? 'visible' : ''}">{error}</div>
+
+<main>
+    {#if data != null}
+
+    <section>
+        <h1>Battle of lebabon</h1>
+        <p class="uppercase extra">syria</p>
+    </section>
+    
+    <section>
+        <h2>Campaign Statistics</h2>
+        <div class="row">
+            <div class="card">
+                <div class="card-row">
+                    <h3>Top Fighter Pilot</h3>
+                    <h3>A/A Kills</h3>
+                </div>
+                <div class="card-row">
+                    <div>
+                        {general.topAa.name}
+                    </div>
+                    <div>
+                        {general.topAa.kills}
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-row">
+                    <h3>Top Attacker Pilot</h3>
+                    <h3>A/G Kills</h3>
+                </div>
+                <div class="card-row">
+                    <div>
+                        {general.topAg.name}
+                    </div>
+                    <div>
+                        {general.topAg.kills}
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-row">
+                    <h3>Most Used Airframe</h3>
+                    <h3>Time</h3>
+                </div>
+                <div class="card-row">
+                    <div>
+                        {general.topAirframe.name}
+                    </div>
+                    <div>
+                        {general.topAirframe.time}
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-row">
+                    <h3>Completed Sorties</h3>
+                    <h3>Pilot</h3>
+                </div>
+                <div class="card-row">
+                    <div>
+                        {general.topSorties.amount}
+                    </div>
+                    <div>
+                        {general.topSorties.name}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div class="filter-row">
+            <h2>Pilot Statistics</h2>
+            <div class=filters>
+                <Select items={pilots} itemId="name" label="name" placeholder="All pilots" showChevron />
+                <Select items={Object.values(airframeTimes)} itemId="name" label="name" placeholder="All airframes" showChevron />
+            </div>
+        </div>
+        <table class="hide-mobile">
+            <thead>
+                <tr>
+                    <th class="dim">Pilot</th>
+                    <th class="dim">Total Time Flown</th>
+                    <th class="dim">Airframe</th>
+                    <th class="dim">A/A Kills</th>
+                    <th class="dim">A/G Kills</th>
+                    <th class="dim">Loses</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each pilots as pilot}
+                        <tr>
+                            <td><b>{pilot.name}</b></td>
+                            <td>{pilot.totalTime}</td>
+                            <td>{pilot.airframe}</td>
+                            <td>{pilot.totalAa}</td>
+                            <td>{pilot.totalAg}</td>
+                            <td>{pilot.totalLoses}</td>
+                        </tr>
+                {:else}
+                    <tr>
+                        <td colspan="999">No data</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+
+        <div class="hide-desktop">
+            {#each pilots as pilot}
+            <ul>
+                <li>
+                    <p class="dim">Pilot</p>
+                    <p>{pilot.name}</p>
+                </li>
+                <li>
+                    <p class="dim">Total Time Flown</p>
+                    <p>{pilot.totalTime}</p>
+                </li>
+                <li>
+                    <p class="dim">Airframe</p>
+                    <p>{pilot.airframe}</p>
+                </li>
+                <li>
+                    <p class="dim">A/A Kills</p>
+                    <p>{pilot.totalAa}</p>
+                </li>
+                <li>
+                    <p class="dim">A/G Kills</p>
+                    <p>{pilot.totalAg}</p>
+                </li>
+                <li>
+                    <p class="dim">Losses</p>
+                    <p>{pilot.totalLoses}</p>
+                </li>
+            </ul>
+            {/each}
+            {#if !pilots.length}
+                <ul>
+                    <li>
+                        <p class="dim">No data</p>
+                    </li>
+                </ul>
+            {/if}
+        </div>
+    </section>
+    {:else if error == ""}
+        <h1>Loading...</h1>
+    {/if}
+</main>
+
+{#each toasts as toast (toast.time)}
+    <Toast text={toast.text} time={toast.time} />
+{/each}
+
+<style global>
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+    .neutral {
+        color: #FBCD55;
+    }
+    .red {
+        color: #FB556C;
+    }
+    .blue {
+        color: #55ABFB;
+    }
+    .dim {
+        color: #C8C8C8;
+    }
+    .uppercase {
+        text-transform: uppercase;
+    }
+    body {
+        font-family: "IBM Plex Mono", Arial, Helvetica, sans-serif;
+        color: #fff;
+        background: #222429;
+        line-height: 1.5;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        margin: 0;
+    }
+    h1,h2,h3,h4,p {
+        margin: 0;
+    }
+    main {
+        max-width: 1340px;
+        margin: 0 auto;
+        padding: 40px;
+    }
+    h1 {
+        color: #FA253C;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 31px;
+        text-transform: uppercase;
+    }
+    h2 {
+        font-weight: 700;
+        font-size: 20px;
+        margin-bottom: 16px;
+    }
+    h3 {
+        font-weight: 400;
+        font-size: 14px;
+        color: #C8C8C8;
+        margin-bottom: 4px;
+    }
+    .extra {
+        font-size: 16px;
+    }
+    section {
+        margin-bottom: 40px;
+    }
+    .row {
+        gap: 16px;
+        flex-wrap: wrap;
+        display: flex;
+        justify-content: space-between;
+    }
+    .row + .row {
+        margin-top: 16px;
+    }
+    .card {
+        background: #272A31;
+        border-radius: 4px;
+        padding: 16px;
+        flex: 1 0 21%;
+    }
+    .card-row {
+        display: flex;
+        justify-content: space-between;
+    }
+    footer, th, td {
+        font-weight: 400;
+        text-align: start;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    tr {
+        border-bottom: 12px solid #222429;
+    }
+
+    tbody tr {
+        background: #272A31;
+        border-radius: 4px;
+    }
+
+    td, th {
+        padding: 16px;
+        vertical-align: top;
+    }
+
+    td div + div {
+        margin-top: 16px;
+    }
+    .svelte-select {
+        background: #272A31 !important;
+        border-radius: 0 !important;
+    }
+    .svelte-select {
+        height: 56px;
+        border-color: transparent !important;
+    }
+    .svelte-select-list {
+        background: #272A31 !important;
+    }
+    .svelte-select-list .item {
+        background: #272A31 !important;
+    }
+    .svelte-select-list .item:hover {
+        background: #30333b !important;
+    }
+    ul {
+        list-style: none;
+        padding: 16px;
+        margin: 0;
+        margin-bottom: 16px;
+        background: #272A31;
+    }
+
+    li {
+        display: flex;
+        border-radius: 4px;
+        margin-bottom: 4px;
+    }
+
+    li p:first-child {
+        min-width: 150px;
+    }
+    
+    .flex-column {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .flex-column div + div {
+        margin-top: 6px;
+    }
+
+    .filter-row {
+        display: flex;
+        justify-content: space-between;
+    }
+    .filters {
+        box-sizing: border-box;
+        display: flex;
+        max-width: 60%;
+        width: 100% !important;
+        gap: 16px;
+    }
+    .filters > div {
+        width: 49% !important;
+    }
+    @media ( max-width: 834px ) {
+        main {
+            padding: 40px 16px;
+        }
+        .card {
+            flex: 1 0 100%
+        }
+
+        .coalition .card {
+            flex: 1 0 40%
+        }
+
+        .missions .card {
+            flex: 1 0 21%
+        }
+
+        .filter-row {
+            flex-direction: column;
+            margin-bottom: 20px;
+        }
+        .filters {
+            max-width: 100%;
+        }
+    }
+
+    @media ( max-width: 575px ) {
+        .card {
+            flex: 1 0 100%
+        }
+
+        .missions .card {
+            flex: 1 0 41%
+        }
+        .filters {
+            flex-direction: column;
+        }
+        .filters > div {
+            width: 100% !important;
+        }
+    }
+
+    @media (min-width: 835px) and (max-width: 1225px) {
+        .card {
+            flex: 1 0 41%;
+        }
+    }
+
+    @media (max-width: 670px) {
+        .hide-mobile {
+            display: none;
+        }
+    }
+
+    @media (min-width: 670px) {
+        .hide-desktop {
+            display: none;
+        }
+    }
+
+    @media (min-width: 1600px) {
+        main {
+            max-width: 1600px;
+        }
+
+        .missions .card {
+            flex: 1 0 10%
+        }
+    }
+
+</style>
