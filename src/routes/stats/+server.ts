4b5507c896feb97d5b6849d73879e0cffeb5c413
Dmytro Smirnov <dmytro.s@finixio.com>
2023-05-06 12:49:09 +0300

    Update stats server

--- src/routes/stats/+server.ts
+++ src/routes/stats/+server.ts
@@ -1,19 +1,13 @@
 import { json, redirect, RequestEvent } from "@sveltejs/kit";
+import axios from "axios";
 import parser from "luaparse";
-import fs from "fs";
 
 export async function GET(event: RequestEvent): Promise<Response> {
-
-    const filePath = process.env["SLMOD_DATA_PATH"] || '';
-    const lua = fs.readFileSync(filePath,
-        { encoding: 'utf8', flag: 'r' });
+    const res = await axios.get('https://kestrelsnest.co.ua/luastats');
+    const lua = res.data;
     let ast = await parser.parse(lua, {comments:false});
     const start = ast.body[0].init[0].fields;
     const data = recursiveWalk(start)
-
-    event.setHeaders({
-        "Cache-Control": "max-age=60",
-    });
 
     // Then build a new response
     return json({
